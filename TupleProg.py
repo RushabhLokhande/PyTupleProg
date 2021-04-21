571bf9eeca302db66811c79dab11b6854037ad44
RushabhLokhande <rushabhlokhande7171@gmail.com>
2021-04-21 12:30:28 -0700

    first commit

---
+++ TupleProg.py
@@ -0,0 +1,8 @@
+names =("item1", "item2", "item3", "item4", "item", "item6")
+print(names),
+
+names =("item1", "item2", "item3", "item4", "item5")
+print(names[1])
+
+names =("item1", "item2", "item3", "item4", "item", "item6")
+print(names[-1])
